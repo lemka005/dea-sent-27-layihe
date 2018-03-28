5532e20b5c8c78dcb622984e18b6674f98139984
Yusif Alizada <yusifedu7@gmail.com>
2018-03-28 19:49:11 +0400

    yalizada bezi kodlar yazdi

--- src/com/yalizada/MainClass.java
+++ src/com/yalizada/MainClass.java
@@ -2,11 +2,7 @@ package com.yalizada;
 
 public class MainClass {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		// 
 		System.out.println("bunlari Aydan yazib");
