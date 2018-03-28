7b494e44e055b36059a22ca6f9f18224599d7f9e
Yusif Alizada <yusifedu7@gmail.com>
2018-03-28 16:52:39 +0400

    YAlizada kod yazdi iki ededi cemleyen

--- src/com/yalizada/MainClass.java
+++ src/com/yalizada/MainClass.java
@@ -12,6 +12,17 @@ public class MainClass {
 		System.out.println("bunlari Aydan yazib");
 		
 		
+		
+		// bu kodlari  YAlizada useri yazib
+		
+		// bu proqram iki ededi cemleyir
+		int a=12;
+		int b=33;
+		
+		int cem=a+b;
+		// cem in ekrana yazilmasi
+		System.out.println(cem);
+		
 	}
 
 }
