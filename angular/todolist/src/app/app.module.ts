bdaf2a20de11dcb9473272bb0b410763be123b99
Yusif Alizada <yusifedu@gmail.com>
2020-01-07 21:53:27 -0800

    some

--- angular/todolist/src/app/app.module.ts
+++ angular/todolist/src/app/app.module.ts
@@ -3,17 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import {FormsModule} from '@angular/forms';
+ 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
