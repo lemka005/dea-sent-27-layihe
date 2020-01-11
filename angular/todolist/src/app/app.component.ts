8cd062873a5846e50823e44c4eb0165995d50754
yalizada <37800222+yalizada@users.noreply.github.com>
2020-01-11 02:46:58 -0800

    todos create read delete

--- angular/todolist/src/app/app.component.ts
+++ angular/todolist/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   
+   todo:string='';
+   day:number;
+   register:Date=new Date();
+
+  todos=[
+    {id:1,todo:'learn java',day:12,register:new Date(),status:'not done'},
+    {id:2,todo:'learn html',day:13,register:new Date(),status:'done'},
+    {id:3,todo:'learn css',day:14,register:new Date(),status:'not done'}
+    
+  ];
+ 
+  onSaveTodo(){
+    let id=this.todos.length+1;
+    let todo={id:id,todo:this.todo,day:this.day,register:this.register,status:'not done'};
+this.todos.push(todo);
+  }
+
+  onDeleteTodo(counter:number){
+    this.todos.splice(counter,1);
+  }
 }
