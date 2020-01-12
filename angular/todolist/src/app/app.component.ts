0c4f018cebc2b93911753a458a7b96203de51854
Yusif Alizada <yusifedu@gmail.com>
2020-01-12 13:26:56 -0800

    update step 1

--- angular/todolist/src/app/app.component.ts
+++ angular/todolist/src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
    todo:string='';
    day:number;
    register:Date=new Date();
-
+   status:string='not done';
+   
   todos=[
     {id:1,todo:'learn java',day:12,register:new Date(),status:'not done'},
     {id:2,todo:'learn html',day:13,register:new Date(),status:'done'},
@@ -19,11 +20,22 @@ export class AppComponent {
  
   onSaveTodo(){
     let id=this.todos.length+1;
-    let todo={id:id,todo:this.todo,day:this.day,register:this.register,status:'not done'};
+    let todo={id:id,todo:this.todo,day:this.day,register:this.register,status:this.status};
 this.todos.push(todo);
   }
 
   onDeleteTodo(counter:number){
     this.todos.splice(counter,1);
   }
+
+  onUpdateTodo(counter:number){
+  
+    this.todo=this.todos[counter].todo;
+    this.day=this.todos[counter].day;
+    this.register=this.todos[counter].register;
+    this.status=this.todos[counter].status;
+  
+  
+  }
+
 }
