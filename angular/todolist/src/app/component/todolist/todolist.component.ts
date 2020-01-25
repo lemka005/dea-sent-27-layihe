615fded8383342691b20f43b95f55efc31249dd8
Yusif Alizada <yusifedu@gmail.com>
2020-01-25 14:44:17 +0400

    task model used

--- angular/todolist/src/app/component/todolist/todolist.component.ts
+++ angular/todolist/src/app/component/todolist/todolist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
 
 @Component({
   selector: 'app-todolist',
@@ -7,91 +8,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodolistComponent implements OnInit {
   ngOnInit(): void {
-    console.log('bu komponentin daxilinde olan butun'+ 
-    'deyisenler yarandi ve, siz bu zaman ne'+
-    'is gormek isteyirsinizse bura yazin');
+    console.log('bu komponentin daxilinde olan butun' +
+      'deyisenler yarandi ve, siz bu zaman ne' +
+      'is gormek isteyirsinizse bura yazin');
 
-    this.todosOriginal=this.todos.slice();
+    this.todosOriginal = this.todos.slice();
 
     this.audio.src = "../../assets/ses.wav";
-      this.audio.load();
-  }
-   todo:string='';
-   day:number;
-   register:Date=new Date();
-   status:string='not done';
-   todoId:number=0;
-   index:number=0;
-  todos=[
-    {id:1,todo:'learn java',day:12,register:new Date(),status:'not done'},
-    {id:2,todo:'learn html',day:13,register:new Date(),status:'done'},
-    {id:3,todo:'learn css',day:14,register:new Date(),status:'not done'}
-    
-  ];
+    this.audio.load();
 
-  onSaveTodo(){
-if(this.todoId===0){
- let id=this.todos.length+1;
-    let todo={id:id,todo:this.todo,day:this.day,register:this.register,status:this.status};
-this.todos.push(todo);
-}else{
-  
-  this.todos[this.index].todo=this.todo;
-  this.todos[this.index].day=this.day;
-  this.todos[this.index].register=this.register;
-  this.todos[this.index].status=this.status;
-}
+let t1:Task=new Task(1);
+t1.todo='java';
+t1.day=12;
+let date1:Date=new Date();
+date1.setFullYear(2010);
+t1.register=date1;
+t1.status='done';
 
-this.todosOriginal=this.todos.slice();
-  }
+let t2:Task=new Task(2);
+t2.todo='css';
+t2.day=17;
+let date2:Date=new Date();
+date2.setFullYear(2000);
+t2.register=date2;
+t2.status=' note done';
 
-  onDeleteTodo(counter:number){
-    this.todos.splice(counter,1);
-    this.todosOriginal=this.todos.slice();
-  }
+    this.todos.push(t1,t2);
 
-  onUpdateTodo(counter:number){
-   
-    this.todo=this.todos[counter].todo;
-    this.day=this.todos[counter].day;
-    this.register=this.todos[counter].register;
-    this.status=this.todos[counter].status;
-  this.todoId=this.todos[counter].id;
-  this.index=counter;
   }
-  onResetTodo(){
-    this.todoId=0;
-  }
+   todo:Task=new Task(0);
+  todoId: number = 0;
+  index: number = 0;
+  todos: Task[] = [];
 
-  todosOriginal=[];
-  searchText:string='';
-  search(){
+  onSaveTodo() {
+    if (this.todoId === 0) {
+      let id = this.todos.length + 1;
+      let todo:Task=new Task(id);
+      todo.todo=this.todo.todo;
+      todo.day=this.todo.day;
+      todo.register=this.todo.register;
+      todo.status=this.todo.status;
+
+      this.todos.push(todo);
+    } else {
+
+      this.todos[this.index]  = this.todo;
      
-    let todos=this.todosOriginal.slice();
-this.todos=[];
+    }
+this.todo=new Task(0);
+    this.todosOriginal = this.todos.slice();
+  }
+
+  onDeleteTodo(counter: number) {
+    this.todos.splice(counter, 1);
+    this.todosOriginal = this.todos.slice();
+  }
+
+  onUpdateTodo(counter: number) { 
+    this.todo = this.todos[counter] ; 
+    this.todoId = this.todos[counter].id;
+    this.index = counter;
+  }
+  onResetTodo() {
+    this.todoId = 0;
+  }
+
+  todosOriginal = [];
+  searchText: string = '';
+  search() {
+
+    let todos = this.todosOriginal.slice();
+    this.todos = [];
     for (let index = 0; index < todos.length; index++) {
       const t = todos[index];
-      let s=t.todo+t.day+t.register+t.status;
+      let s = t.todo + t.day + t.register + t.status;
       console.log(s);
-      if(s.includes(this.searchText)){
+      if (s.includes(this.searchText)) {
         this.todos.push(t);
       }
     }
 
   }
-    audio = new Audio();
-  seslendir(){
-    
-     
-      
-      this.audio.play();
-   
+  audio = new Audio();
+  seslendir() {
+
+
+
+    this.audio.play();
+
   }
-  sesiDayandir(){
+  sesiDayandir() {
     this.audio.pause();
   }
-  sesiDayandir2(){
-  this.audio.load();
-  this.audio.play();
+  sesiDayandir2() {
+    this.audio.load();
+    this.audio.play();
   }
 }
