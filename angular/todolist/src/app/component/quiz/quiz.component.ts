6c11bd50cda8f44cc65189192ccb68d6b7965868
yalizada <37800222+yalizada@users.noreply.github.com>
2020-02-01 13:37:04 +0400

    see result step 1

--- angular/todolist/src/app/component/quiz/quiz.component.ts
+++ angular/todolist/src/app/component/quiz/quiz.component.ts
@@ -16,5 +16,22 @@ export class QuizComponent implements OnInit {
 this.questions=this.questionService.questions.slice();
   }
 
-  
+  onSeeResults(){
+    let totalPoint:number=0;
+
+    for (let index = 0; index < this.questions.length; index++) {
+      const q = this.questions[index];
+    
+      for (let index2 = 0; index2 < q.answers.length; index2++) {
+        const a = q.answers[index2];
+        if(a.correct && a.answer==q.studentAnswer){
+          totalPoint++; break;
+        }
+        
+      }
+    }
+
+    alert('neticen = '+totalPoint);
+
+  }
 }
