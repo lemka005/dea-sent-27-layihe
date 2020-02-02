f348a643109be61e1fef6b12a292cc6e338b9fee
yalizada <37800222+yalizada@users.noreply.github.com>
2020-02-02 14:04:45 +0400

    Update app-routing.module.ts

--- angular/todolist/src/app/app-routing.module.ts
+++ angular/todolist/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { LogoutComponent } from './component/logout/logout.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { ProductsComponent } from './component/products/products.component';
+import { OrdersComponent } from './component/orders/orders.component';
 
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
-  {path:'logout',component:LogoutComponent}
+  {path:'logout',component:LogoutComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'orders',component:OrdersComponent},
+  {path:'**',component:LoginComponent}
 ];
 
 @NgModule({
