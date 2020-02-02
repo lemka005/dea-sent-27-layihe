f2b17b1eda50a0a6ca91d4422cdda2d772d77933
yalizada <37800222+yalizada@users.noreply.github.com>
2020-02-02 13:59:29 +0400

    routing configured

--- angular/todolist/src/app/app-routing.module.ts
+++ angular/todolist/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { LogoutComponent } from './component/logout/logout.component';
 
 
 const routes: Routes = [
-  
+  {path:'login',component:LoginComponent},
+  {path:'logout',component:LogoutComponent}
 ];
 
 @NgModule({
